421644023cd941d9f11e0fa14c5fa4c834661d84
Ragnar-Oock <u.drgnk@outlook.com>
2021-04-16 19:48:33 +0200

    add a basic button component

---
+++ src/components/SButton.vue
@@ -0,0 +1,33 @@
+<template>
+	<button class="btn">
+		<span class="icon"><slot name="icon" /></span>
+		<span class="btn__text"><slot /></span>
+	</button>
+</template>
+
+<script>
+export default {
+	name: 'SButton',
+	props: {
+		iconSide: {
+			type: String,
+			default: 'left',
+			required: false,
+			validator(value) {
+				return ['left', 'right'].includes(value);
+			}
+		}
+	},
+	data () {
+		return {
+
+		};
+	}
+};
+</script>
+
+<style scoped lang="scss">
+  .btn {
+		color: red
+  }
+</style>
