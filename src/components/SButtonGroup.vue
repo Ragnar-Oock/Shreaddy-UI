98fac68fea7020a803db5b9a194f28af8bced010
Ragnar-Oock <u.drgnk@outlook.com>
2021-04-17 02:53:56 +0200

    add SButtonGroup component

---
+++ src/components/SButtonGroup.vue
@@ -0,0 +1,54 @@
+<template>
+	<div
+		class="s-button-group"
+		:class="[type]"
+	>
+		<slot />
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'SButtonGroup',
+	props: {
+		type: {
+			type: String,
+			default: 'normal',
+			required: false,
+			validator(value) {
+				return ['normal', 'compact', 'menu'].includes(value);
+			}
+		},
+		direction: {
+			type: String,
+			default: 'vertical',
+			required: false,
+			validator(value) {
+				return ['vertical', 'horizontal'].includes(value);
+			}
+		}
+	}
+};
+
+
+</script>
+
+<style lang="scss">
+  .s-button-group {
+		display: flex;
+		padding: .5em;
+		background-color: var(--filler-6);
+		border-radius: 5px;
+		box-shadow: var(--shadow);
+		width: max-content;
+		max-width: 45ch;
+		flex-direction: column;
+
+		&.compact {
+			flex-direction: row;
+			padding: 0;
+			overflow: hidden;
+		}
+  }
+</style>
