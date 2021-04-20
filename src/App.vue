75e2d0715e28909c71e69bc333b98668af9f2437
Ragnar-Oock <u.drgnk@outlook.com>
2021-04-20 15:12:20 +0200

    change playground from buttons to radios

--- src/App.vue
+++ src/App.vue
@@ -1,394 +1,64 @@
 <template>
-	<div class="grid">
-		<SButton
-			icon-side="right"
-		>
-			<template #default>
-				{{ buttonText }}
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-		<SButton
-			icon-side="left"
-		>
-			<template #default>
-				{{ buttonText }}
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-		<SButton
-			icon-side="right"
-			:disabled="true"
-		>
-			<template #default>
-				{{ buttonText }}
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-		<SButton
-			icon-side="left"
-			:disabled="true"
-		>
-			<template #default>
-				{{ buttonText }}
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
+	<SRadio
+		id="radio1"
+		name="radio1"
+		value="1"
+	>
+		I'm a radio button
+	</SRadio>
+	<SRadio
+		id="radio2"
+		name="radio1"
+		value="2"
+		:disabled="true"
+	>
+		I'm a radio button
+	</SRadio>
+	<SRadio
+		id="radio3"
+		name="radio1"
+		value="3"
+	>
+		I'm a radio button
+	</SRadio>
 
-		<SButton
-			icon-side="right"
-			:dull="true"
-		>
-			<template #default>
-				{{ buttonText }}
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-		<SButton
-			icon-side="left"
-			:dull="true"
-		>
-			<template #default>
-				{{ buttonText }}
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-
-		<SButton
-			icon-side="right"
-			:small="true"
-		>
-			<template #default>
-				{{ buttonText }}
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-		<SButton
-			icon-side="left"
-			:small="true"
-		>
-			<template #default>
-				{{ buttonText }}
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-
-		<SButton
-			icon-side="right"
-			:small="true"
-		>
-			<template #default>
-				{{ buttonText }}
-			</template>
-		</SButton>
-		<SButton
-			icon-side="left"
-			:small="true"
-		>
-			<template #default>
-				{{ buttonText }}
-			</template>
-		</SButton>
-
-		<SButton
-			icon-side="right"
-			:block="true"
-		>
-			<template #default>
-				{{ buttonText }}
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-		<SButton
-			icon-side="left"
-			:block="true"
-		>
-			<template #default>
-				{{ buttonText }}
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-	</div>
-	<SButtonGroup type="compact">
-		<SButton
-			icon-side="left"
-			:compact="true"
-		>
-			<template #default>
-				Edit chances
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-		<SButton
-			icon-side="left"
-			:compact="true"
-			:danger="true"
-			tooltip="I'm a dangerous button"
-		>
-			<template #default>
-				Randomise title
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-	</SButtonGroup>
-	<SButtonGroup type="menu">
-		<SButton
-			icon-side="left"
-		>
-			<template #default>
-				Edit chances
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-		<SButton
-			icon-side="left"
-			:danger="true"
-		>
-			<template #default>
-				Randomise title
-			</template>
-			<template #icon>
-				<svg
-					version="1.1"
-					viewBox="0 0 16 9.5"
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					aria-hidden="true"
-				><path
-					class="a"
-					d="M 1.5,1.5 8,8 14.5,1.5"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1.5"
-				/></svg>
-			</template>
-		</SButton>
-	</SButtonGroup>
+	<SRadioGroup
+		direction="horizontal"
+		type="block"
+		name="radio-group"
+		:options="getOptions"
+	/>
 </template>
 
 <script>
-import SButton from './components/SButton.vue';
-import SButtonGroup from './components/SButtonGroup.vue';
+import SRadio from './components/SRadio.vue';
+import SRadioGroup from './components/SRadioGroup.vue';
 
 export default {
 	components:{
-		SButton,
-		SButtonGroup
+		SRadio,
+		SRadioGroup
 	},
 	data() {
 		return {
 			buttonText: 'Some text here'
 		};
+	},
+	computed: {
+		getOptions() {
+			return [
+				{
+					label: this.buttonText,
+					value: '1',
+					isDisabled: false
+				},
+				{
+					label: 'lorem ipsum',
+					value: '2',
+					isDisabled: false
+				}
+			];
+		}
 	}
 };
 </script>
@@ -397,7 +67,6 @@ export default {
 #app {
 	font-family: 'Quicksand';
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 
