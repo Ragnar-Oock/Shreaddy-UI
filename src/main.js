f8a67b13559ca89e86683454176e391061f523e3
Ragnar-Oock <u.drgnk@outlook.com>
2021-04-21 17:11:06 +0200

    add css reset normalize

--- src/main.js
+++ src/main.js
@@ -8,4 +8,6 @@ const app = createApp(App);
 import tippyPlugin from './helpers/tippy-plugin';
 app.use(tippyPlugin);
 
+import 'css-reset-and-normalize/scss/reset-and-normalize.scss';
+
 app.mount('#app');
