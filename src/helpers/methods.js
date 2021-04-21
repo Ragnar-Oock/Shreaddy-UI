449e8b7f08ee641bad45b8a3314a877081943087
Ragnar-Oock <u.drgnk@outlook.com>
2021-04-21 18:35:41 +0200

    add a calculate text with helper function

--- src/helpers/methods.js
+++ src/helpers/methods.js
@@ -12,3 +12,15 @@ export function getUuid() {
 		return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, b);
 	}();
 }
+
+export function calculateTextWidth(text, font = '16px times', unit = 'px') {
+	// if given, use cached canvas for better performance
+	// else, create new canvas
+	var canvas = calculateTextWidth.canvas || (calculateTextWidth.canvas = document.createElement('canvas'));
+	var context = canvas.getContext('2d');
+
+	context.font = font;
+	var metrics = context.measureText(text);
+
+	return `${ Math.ceil(metrics.width) }${ unit }`;
+}
